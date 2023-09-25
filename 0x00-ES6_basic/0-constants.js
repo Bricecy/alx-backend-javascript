9e5b3544878c5ccc50e07ce3216a20b114f4c9a0
Bricecy <brigidwambua97@gmail.com>
2023-09-25 05:12:30 +0000

    Task0

---
+++ 0x00-ES6_basic/0-constants.js
@@ -0,0 +1,15 @@
+function taskFirst() {
+  const firstName = "John";
+  const lastName = "Doe";
+  console.log(`Hello, ${firstName} ${lastName}`);
+}
+
+function taskNext() {
+  let count = 0;
+  count = count + 1;
+  console.log(`Count: ${count}`);
+}
+
+taskFirst(); // Output: Hello, John Doe
+taskNext();  // Output: Count: 1
+
